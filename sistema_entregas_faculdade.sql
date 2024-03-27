7d4adcffc4f5b36f02ea8026056c6488532d1496
KarinaSampaio <132115526+KarinaSampaio@users.noreply.github.com>
2024-03-27 17:22:25 -0300

    Update sistema_entregas_faculdade.sql

--- sistema_entregas_faculdade.sql
+++ sistema_entregas_faculdade.sql
@@ -1,9 +1,37 @@
-use sistema_entregas;
+CREATE DATABASE sistema_entregas;
+USE sistema_entregas;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE locais (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL
+);
+
+INSERT INTO usuarios (nome, email, senha) VALUES
+('Karina Sampaio', 'karinasampaio13@gmail.com', 'senha1234'),
+('Izabel Sampaio', 'izabel-sampaio@hotmail.com', 'senhabel123'),
+('Beyonce Knowles', 'beyoncediva@gmail,com', 'senhacrazyinlove123'),
+('Taylor Swift', 'taylornation@gmail,com', 'senhatheerastour');
+
+INSERT INTO locais (id, nome, endereco) Values
+('1', 'Karina Sampaio', 'Rio de Janeiro Brasil'),
+('2', 'Izabel Sampio', 'Rio de Janeiro Brasil'),
+('3', 'Beyonce Knowles', 'Texas, EUA'),
+('4', 'Taylor Swift', 'Nova York, Eua'); 
+
 
 select *
-from usuarios;
+from usuarios
+join locais on usuarios.nome=locais.nome;
 
-INSERT INTO usuarios (nome, email, senha) VALUES ('Karina Sampaio', 'karinasampaio13@gmail.com', '12345');
-INSERT INTO usuarios (nome, email, senha) values ('Izabel Sampaio', 'izabel-sampaio@hotmail.com', 'bel123'); 
+select nome, endereco
+from locais;
 
 
